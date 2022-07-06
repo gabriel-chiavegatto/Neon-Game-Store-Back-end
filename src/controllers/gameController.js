246a717fb142c8cef3752ca3dfca81432b020214
samer <samervalente@gmail.com>
2022-07-06 19:10:38 -0300

    feat: create and list games routes

---
+++ src/controllers/gameController.js
@@ -0,0 +1,26 @@
+import {db, ObjectId} from "../dbStrategy.js/mongo.js"
+
+
+async function RegisterGame(req, res){
+ const gameData = req.body
+ //name, description, category, price, imageURL
+ try {
+    await db.collection("games").insertOne(gameData)
+    res.status(201).send(gameData)
+
+ } catch (error) {
+    return res.sendStatus(500)
+ }
+}
+
+async function getGames(req, res){
+   try {
+      const games = await db.collection("games").find().toArray()
+      res.status(200).send(games)
+   } catch (error) {
+      return res.sendStatus(500)
+   }
+}
+
+
+export {RegisterGame, getGames}
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,9 +1,10 @@
 import {Router} from "express"
+import { RegisterGame, getGames } from "../controllers/gameController.js"
 
 const route = Router()
 
-route.post("/game")
-route.get("/games")
+route.post("/game", RegisterGame)
+route.get("/games", getGames)
 
 export default route
 
