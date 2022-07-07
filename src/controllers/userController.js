5d0e7c99ba633f558a9a4cac3907781d961b9567
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-07-07 16:51:40 -0300

    new feature

---
+++ src/controllers/userController.js
@@ -0,0 +1,9 @@
+import {db, ObjectId} from "../dbStrategy.js/mongo.js";
+
+async function signIn(req, res){
+
+}
+
+async function signOn(req,res){
+    
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import cors from "cors";
 import productRoute from "./routes/productRoute.js";
+import userRoute from "./routes/userRoute.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -10,6 +11,8 @@ server.use(cors());
 server.use(json());
 
 server.use(productRoute);
+server.use(userRoute);
+
 console.log("hello world");
 
 const PORT = process.env.PORT;
